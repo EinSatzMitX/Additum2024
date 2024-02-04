793bb1e5a8c937e941f292065a1ad63e2b8d1e20
DasWarWohlNix <PeterFreder@outlook.de>
2024-02-04 20:13:47 +0100

    working clearScreen() added

---
+++ Rpi4OS/src/breakout.h
@@ -0,0 +1,20 @@
+struct Object
+{
+    unsigned int type;
+    unsigned int x;
+    unsigned int y;
+    unsigned int width;
+    unsigned int height;
+    unsigned char alive;
+};
+
+void removeObject(struct Object *object);
+void moveObject(struct Object *object, int xoff, int yoff);
+struct Object *detectCollision(struct Object *with, int xoff, int yoff);
+unsigned char getUart();
+void initBricks();
+void initBall();
+void initPaddle();
+void drawScoreboard(int score, int lives);
+void clearScreen();
+void initBreakout();
